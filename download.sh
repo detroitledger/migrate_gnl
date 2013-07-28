600a5ed74b7c0a3f0e1a6347066f9e0f4eb1468a
Benjamin Chodoroff <ben@theworkdept.com>
2013-07-28 18:29:41 -0400

    improve downloader w/ auto-id creation, modify migrate to work a little better

--- download.sh
+++ download.sh
@@ -21,5 +21,17 @@ do
   $WGET -O $DESTDIR/grants$SHEET.csv "$URL"
 done
 
-cat $DESTDIR/grants*.csv | grep -v 'granter,granter_program,grantee,grantee_city,grantee_state,impact_area,impact_neighborhood,amount,year,length_years,data_source,notes' > $DESTDIR/allgrants.csv
+# concatenate
+cat $DESTDIR/grants*.csv | grep -v 'granter,granter_program,grantee,grantee_city,grantee_state,impact_area,impact_neighborhood,amount,year,length_years,data_source,notes' > $DESTDIR/allgrants-raw.csv
+
+# add empty id field
+sed 's/^/,/' $DESTDIR/allgrants-raw.csv > $DESTDIR/allgrants-raw-emptyfirst.csv
+
+# add autoincrement to empty id field
+awk -F, '$1=NR' OFS=, $DESTDIR/allgrants-raw-emptyfirst.csv > $DESTDIR/allgrants-raw-incremented.csv
+
+# add header row
+echo 'id,granter,granter_program,grantee,grantee_city,grantee_state,impact_area,impact_neighborhood,amount,year,length_years,data_source,notes' > $DESTDIR/allgrants.csv
+
+cat $DESTDIR/allgrants-raw-incremented.csv >> $DESTDIR/allgrants.csv
 
