667c628b38b09d3c7f1192c8eab74eb4bcd95549
Benjamin Chodoroff <ben@theworkdept.com>
2013-08-31 15:13:34 -0400

    store granter program, impact neighborhood and project in tags

--- download.sh
+++ download.sh
@@ -15,7 +15,7 @@ $WGET -O $DESTDIR/classify_nonprofits_detail.csv "${CLASSIFICATIONS_BASE}1"
 
 $WGET -O $DESTDIR/classify_grants.csv "${CLASSIFICATIONS_BASE}0"
 
-for SHEET in 0 1 2 3 5 6 7 9 10 12 13 14 15 17 19 20 21 22 23
+for SHEET in 0 1 2 3 5 6 7 9 10 12 13 14 15 17 19 20 21 22 23 24
 do
   URL=$GRANTS_BASE$SHEET
   $WGET -O $DESTDIR/grants$SHEET.csv "$URL"
@@ -23,7 +23,7 @@ do
 done
 
 # concatenate
-cat $DESTDIR/grants*.csv | grep -v 'granter,granter_program,grantee,grantee_city,grantee_state,impact_area,impact_neighborhood,amount,year,length_years,data_source,notes' > $DESTDIR/allgrants-raw.csv
+cat $DESTDIR/grants*.csv | grep -v 'granter,granter_program,grantee,grantee_city,grantee_state,impact_area,impact_neighborhood,amount,year,length_years,data_source,notes,project_tags' > $DESTDIR/allgrants-raw.csv
 
 # add empty id field
 sed 's/^/,/' $DESTDIR/allgrants-raw.csv > $DESTDIR/allgrants-raw-emptyfirst.csv
@@ -32,7 +32,7 @@ sed 's/^/,/' $DESTDIR/allgrants-raw.csv > $DESTDIR/allgrants-raw-emptyfirst.csv
 awk -F, '$1=NR' OFS=, $DESTDIR/allgrants-raw-emptyfirst.csv > $DESTDIR/allgrants-raw-incremented.csv
 
 # add header row
-echo 'id,granter,granter_program,grantee,grantee_city,grantee_state,impact_area,impact_neighborhood,amount,year,length_years,data_source,notes' > $DESTDIR/allgrants.csv
+echo 'id,granter,granter_program,grantee,grantee_city,grantee_state,impact_area,impact_neighborhood,amount,year,length_years,data_source,notes,project_tags' > $DESTDIR/allgrants.csv
 
 cat $DESTDIR/allgrants-raw-incremented.csv >> $DESTDIR/allgrants.csv
 
